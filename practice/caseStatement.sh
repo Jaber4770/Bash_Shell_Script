fb5fa94eed49880dd81429484e3f0c357946aaa6
unknown <Jaber Ahmed>
2024-01-01 20:39:42 +0200

    case statement in bash shell

---
+++ practice/caseStatement.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+fruit="orange"
+
+case $fruit in
+    "apple")
+        echo "This is a red fruit."
+        ;;
+    "banana")
+        echo "This is a yellow fruit."
+        ;;
+    "orange")
+        echo "This is an orange fruit."
+        ;;
+    *)
+        echo "It is an unknown fruite."
+        ;;
+esac
