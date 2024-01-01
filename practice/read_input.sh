63644fd644419c12eaae6f336157a1a5b83d7dee
unknown <Jaber Ahmed>
2024-01-01 20:03:20 +0200

    if else condition

--- practice/read_input.sh
+++ practice/read_input.sh
@@ -3,6 +3,10 @@ do
   echo $line
 done < input.txt
 
-# add text to a file.
+# add text to a file is overwrite.
 
-echo "This is some text." > output.txt
+echo "This is some text." > input.txt
+
+#adding more text in a file where already many data are already stored.
+
+echo "this is new text which will be added in a exits file where is text already availabe." >> input.txt
